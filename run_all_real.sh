ebd02a7383f15ae84949420c408c0ba47a3404c6
Panagiotis Bouros <12070531+pbour@users.noreply.github.com>
2023-10-05 21:38:09 +0200

    Add files via upload

---
+++ run_all_real.sh
@@ -0,0 +1,40 @@
+bash run_real_vary-qe.sh BOOKS 10 query $1
+bash run_real_vary-qe.sh BOOKS 10 query $1 sorted
+bash run_real_vary-qe.sh BOOKS 10 level $1 sorted
+bash run_real_vary-qe.sh BOOKS 10 partition $1 sorted
+
+bash run_real_vary-qe.sh WEBKIT 12 query $1
+bash run_real_vary-qe.sh WEBKIT 12 query $1 sorted
+bash run_real_vary-qe.sh WEBKIT 12 level $1 sorted
+bash run_real_vary-qe.sh WEBKIT 12 partition $1 sorted
+
+bash run_real_vary-qe.sh TAXIS 17 query $1
+bash run_real_vary-qe.sh TAXIS 17 query $1 sorted
+bash run_real_vary-qe.sh TAXIS 17 level $1 sorted
+bash run_real_vary-qe.sh TAXIS 17 partition $1 sorted
+
+bash run_real_vary-qe.sh GREEND 17 query $1
+bash run_real_vary-qe.sh GREEND 17 query $1 sorted
+bash run_real_vary-qe.sh GREEND 17 level $1 sorted
+bash run_real_vary-qe.sh GREEND 17 partition $1 sorted
+
+
+bash run_real_vary-qn.sh BOOKS 10 query $1
+bash run_real_vary-qn.sh BOOKS 10 query $1 sorted
+bash run_real_vary-qn.sh BOOKS 10 level $1 sorted
+bash run_real_vary-qn.sh BOOKS 10 partition $1 sorted
+
+bash run_real_vary-qn.sh WEBKIT 12 query $1
+bash run_real_vary-qn.sh WEBKIT 12 query $1 sorted
+bash run_real_vary-qn.sh WEBKIT 12 level $1 sorted
+bash run_real_vary-qn.sh WEBKIT 12 partition $1 sorted
+
+bash run_real_vary-qn.sh TAXIS 17 query $1
+bash run_real_vary-qn.sh TAXIS 17 query $1 sorted
+bash run_real_vary-qn.sh TAXIS 17 level $1 sorted
+bash run_real_vary-qn.sh TAXIS 17 partition $1 sorted
+
+bash run_real_vary-qn.sh GREEND 17 query $1
+bash run_real_vary-qn.sh GREEND 17 query $1 sorted
+bash run_real_vary-qn.sh GREEND 17 level $1 sorted
+bash run_real_vary-qn.sh GREEND 17 partition $1 sorted
---
+++ run_all_synthetic.sh
@@ -0,0 +1,29 @@
+bash run_synthetic_vary-d.sh 17 query 10
+bash run_synthetic_vary-d.sh 17 query 10 sorted
+bash run_synthetic_vary-d.sh 17 level 10 sorted
+bash run_synthetic_vary-d.sh 17 partition 10 sorted
+
+bash run_synthetic_vary-c.sh 17 query 10
+bash run_synthetic_vary-c.sh 17 query 10 sorted
+bash run_synthetic_vary-c.sh 17 level 10 sorted
+bash run_synthetic_vary-c.sh 17 partition 10 sorted
+
+bash run_synthetic_vary-a.sh 17 query 10
+bash run_synthetic_vary-a.sh 17 query 10 sorted
+bash run_synthetic_vary-a.sh 17 level 10 sorted
+bash run_synthetic_vary-a.sh 17 partition 10 sorted
+
+bash run_synthetic_vary-s.sh 17 query 10
+bash run_synthetic_vary-s.sh 17 query 10 sorted
+bash run_synthetic_vary-s.sh 17 level 10 sorted
+bash run_synthetic_vary-s.sh 17 partition 10 sorted
+
+bash run_synthetic_vary-qe.sh 17 query 10
+bash run_synthetic_vary-qe.sh 17 query 10 sorted
+bash run_synthetic_vary-qe.sh 17 level 10 sorted
+bash run_synthetic_vary-qe.sh 17 partition 10 sorted
+
+bash run_synthetic_vary-qn.sh 17 query 10
+bash run_synthetic_vary-qn.sh 17 query 10 sorted
+bash run_synthetic_vary-qn.sh 17 level 10 sorted
+bash run_synthetic_vary-qn.sh 17 partition 10 sorted
