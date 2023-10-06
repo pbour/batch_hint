346136a4e30911987696e538e85e46670a168563
Panagiotis Bouros <12070531+pbour@users.noreply.github.com>
2023-10-06 16:40:19 +0200

    Update run_synthetic_vary-qn.sh

--- run_synthetic_vary-qn.sh
+++ run_synthetic_vary-qn.sh
@@ -14,11 +14,11 @@ b=$(echo $2 | tr a-z A-Z)
 for qn in 5 10 50 100
 do
         qfile="queries/synthetic/synthetic_qe0.1%_qn${qn}K.qry"
-        if [ "$#" -eq 4 ]; then
+        if [ "$#" -eq 3 ]; then
                 ofile="outputs/synthetic/${prefix}_qe0.1%_qn${qn}K_oSUBS+SORT+SS+CM_m$1_qGOVERLAPS_b${b}.out"
                 echo "./query_batch.exec -m $1 -b $b -r $3 $ifile $qfile > $ofile"
                 ./query_batch.exec -m $1 -b $2 -r $3 $ifile $qfile > $ofile
-        elif [ "$#" -eq 5 ] && ([ $4 = "SORTED" ] || [ $4 = "sorted" ]); then
+        elif [ "$#" -eq 4 ] && ([ $4 = "SORTED" ] || [ $4 = "sorted" ]); then
                 ofile="outputs/synthetic/${prefix}_qe0.1%_qn${qn}K_oSUBS+SORT+SS+CM_m$1_qGOVERLAPS_b${b}_withSORT.out"
                 echo "./query_batch.exec -m $1 -b $b -r $3 -s $ifile $qfile > $ofile"
                 ./query_batch.exec -m $1 -b $2 -r $3 -s $ifile $qfile > $ofile
